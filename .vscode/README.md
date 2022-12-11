b1690e51d0966b964deadea96faf731b2472d0ea
RajeshRk18 <kannar432@gmail.com>
2022-12-11 21:56:32 +0530

    README.md

---
+++ .vscode/README.md
@@ -0,0 +1,1 @@
+A dupe blockchain!
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     //Reward for validating a block
-    const REWARD: u8= 50;
+    const REWARD: u8 = 50;
 
     //Structure of a blockchain
     struct BlockChain {
@@ -15,7 +15,7 @@ fn main() {
     }
     #[derive(Debug)]
     //Defining the structure of a block
-    struct Block{
+    struct Block {
         block_header: String,
         block_no: u32,
         previous_hash: String,
@@ -25,14 +25,15 @@ fn main() {
     }
 
     //implementation for block
-    impl Block{
-        fn new(block_header: String,
+    impl Block {
+        fn new(
+            block_header: String,
             block_no: u32,
             previous_hash: String,
             txn_data: Vec<TxnType>,
             nonce: u64,
-            difficulty: u64) -> Block{
-
+            difficulty: u64,
+        ) -> Block {
             Block {
                 block_header,
                 block_no,
@@ -44,8 +45,7 @@ fn main() {
         }
     }
 
-    fn add_block(block: Block, blockchain: &mut BlockChain) ->&mut BlockChain{
-
+    fn add_block(block: Block, blockchain: &mut BlockChain) -> &mut BlockChain {
         blockchain.blocks.push(block);
 
         blockchain
@@ -60,17 +60,14 @@ fn main() {
     }
 
     //implementing coinbase txn
-    impl CoinbaseTxn{
-        fn new(amount: u8, validator: String) -> CoinbaseTxn{
-            CoinbaseTxn {
-                amount,
-                validator,
-            }
+    impl CoinbaseTxn {
+        fn new(amount: u8, validator: String) -> CoinbaseTxn {
+            CoinbaseTxn { amount, validator }
         }
     }
 
     #[derive(Debug)]
-    struct Txn{
+    struct Txn {
         sender: String,
         receiver: String,
         amount: u32,
@@ -79,8 +76,8 @@ fn main() {
 
     //implementing txn
 
-    impl Txn{
-        fn new(sender: String, receiver: String, amount: u32, gas_fees: f32) -> Txn{
+    impl Txn {
+        fn new(sender: String, receiver: String, amount: u32, gas_fees: f32) -> Txn {
             Txn {
                 sender,
                 receiver,
@@ -92,15 +89,17 @@ fn main() {
     #[derive(Debug)]
     enum TxnType {
         Txn(Txn),
-        CoinbaseTxn(CoinbaseTxn)
+        CoinbaseTxn(CoinbaseTxn),
     }
 
-    fn hash(block: &Block)->String{
+    fn hash(block: &Block) -> String {
         use sha256::digest;
-        let input = format!("{}{}{:?}{:?}{}",block.block_header, block.block_no, block.txn_data, block.previous_hash, block.nonce);
-        
+        let input = format!(
+            "{}{}{:?}{:?}{}",
+            block.block_header, block.block_no, block.txn_data, block.previous_hash, block.nonce
+        );
+
         digest(input)
-    
     }
 
     //New blockchain instance
@@ -108,20 +107,23 @@ fn main() {
 
     // block0
     let previous_hash = String::from("00000000000000000000000");
-    let mut txns: Vec<TxnType> = vec![];    
-    let coinbase_txn = CoinbaseTxn{amount: REWARD, validator: "Validator1".to_string()};
+    let mut txns: Vec<TxnType> = vec![];
+    let coinbase_txn = CoinbaseTxn {
+        amount: REWARD,
+        validator: "Validator1".to_string(),
+    };
 
     txns.push(TxnType::CoinbaseTxn(coinbase_txn));
 
     let block = Block::new("Block0".to_string(), 0, previous_hash, txns, 0, 0);
-    let previous_hash = hash(&block);    
+    let previous_hash = hash(&block);
     add_block(block, &mut blockchain);
 
     // block1
     let mut txns: Vec<TxnType> = vec![];
     let txn1 = Txn::new("Anon1".to_string(), "Anon2".to_string(), 10, 0.05);
     let txn2 = Txn::new("Anon2".to_string(), "Anon3".to_string(), 8, 0.04);
-    let txn3 = Txn::new("Anon3".to_string(),"Anon1".to_string(), 5, 0.02);
+    let txn3 = Txn::new("Anon3".to_string(), "Anon1".to_string(), 5, 0.02);
     let coinbase_txn = CoinbaseTxn::new(50, "Validator2".to_string());
 
     txns.push(TxnType::Txn(txn1));
@@ -130,11 +132,10 @@ fn main() {
     txns.push(TxnType::CoinbaseTxn(coinbase_txn));
 
     let block = Block::new("Block0".to_string(), 0, previous_hash, txns, 0, 0);
-    let current_hash = hash(&block);    
+    let current_hash = hash(&block);
     add_block(block, &mut blockchain);
 
     // block2
 
     println!("{}", &current_hash);
-
-    }       
+}
